ccfb10a1bfd5f846eea7a172ce99dc26b8d466a3
nanyansky <57956533+nanyansky@users.noreply.github.com>
2025-12-28 16:16:14 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/base/App.java
@@ -188,16 +188,16 @@ public class App extends MultiDexApplication {
 
         // 不添加默认线路
         // 默认线路地址
-        // String defaultApiName = "自备份线路";
-        // String defaultApi = url + URL.DEFAULT_API_URL;
-        // Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
-        // defaultApiMap.put(defaultApiName, defaultApi);
-        // List<String> defaultApiHistory = Hawk.get(HawkConfig.API_NAME_HISTORY, new ArrayList<>());
-        // defaultApiHistory.add(defaultApiName);
-        // putDefault(HawkConfig.API_URL, defaultApi);
-        // putDefault(HawkConfig.API_NAME, defaultApiName);
-        // putDefault(HawkConfig.API_NAME_HISTORY, defaultApiHistory);
-        // putDefault(HawkConfig.API_MAP, defaultApiMap);
+        String defaultApiName = "自备份线路";
+        String defaultApi = url + URL.DEFAULT_API_URL;
+        Map<String, String> defaultApiMap = Hawk.get(HawkConfig.API_MAP, new HashMap<>());
+        defaultApiMap.put(defaultApiName, defaultApi);
+        List<String> defaultApiHistory = Hawk.get(HawkConfig.API_NAME_HISTORY, new ArrayList<>());
+        defaultApiHistory.add(defaultApiName);
+        putDefault(HawkConfig.API_URL, defaultApi);
+        putDefault(HawkConfig.API_NAME, defaultApiName);
+        putDefault(HawkConfig.API_NAME_HISTORY, defaultApiHistory);
+        putDefault(HawkConfig.API_MAP, defaultApiMap);
         // 添加默认直播源
         putDefault(HawkConfig.LIVE_URL, URL.DEFAULT_LIVE_URL);
 
